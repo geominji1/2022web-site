9fa1b9870b8de6bbb7f8b534d03744e2671c02d1
geominji1 <heominji1819@gmail.com>
2022-07-28 16:29:41 +0900

    20220728

--- 07cu/js/script.js
+++ 07cu/js/script.js
@@ -95,13 +95,16 @@ $(document).ready(function(){
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
+    autoplay:true,
     centeredSlides: true,
     loop: true,
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    
 });
